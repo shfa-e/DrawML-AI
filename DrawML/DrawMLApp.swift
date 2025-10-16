12cd3a44aaeac2a61857a3b516cac7517c751ba6
shfa <shefaa.eidaroos@gmail.com>
2025-10-16 22:01:55 +0300

    Initial Commit

---
+++ DrawML/DrawMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawMLApp.swift
+//  DrawML
+//
+//  Created by Whyyy on 16/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct DrawMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
